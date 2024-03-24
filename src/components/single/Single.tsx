09767acf28ab26c5c1693e2ccf6e6cd12f4f97c8
dinod tharinda <dinodtharinda2001@gmail.com>
2024-03-24 14:35:24 +0530

    change product page file

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -1,176 +1,176 @@
-import React, { useEffect, useState } from "react";
-import "./single.scss";
-import { PRODUCT_BASE_URL } from "../../data";
-import { getData ,postData, putData } from "../API/HttpService";
-import { IconButton, Snackbar } from "@mui/material";
-import Base64ToImageConverter from "../utills/BaseToImage";
+// import React, { useEffect, useState } from "react";
+// import "./single.scss";
+// import { PRODUCT_BASE_URL } from "../../data";
+// import { getData ,postData, putData } from "../API/HttpService";
+// import { IconButton, Snackbar } from "@mui/material";
+// import Base64ToImageConverter from "../utills/BaseToImage";
 
-type Props = {
-  id: string;
-};
+// type Props = {
+//   id: string;
+// };
 
-export const Single = (props: Props) => {
-  const [product, setProduct] = useState({
-    id: "",
-    name:"",
-    code: "",
-    category_id: "",
-    price: "",
-    cost:"",
-    qty: "",
-    alert_quantity: "",
-    image: "",
-    description: "",
-    is_active: false,
-    created_at: "",
-    updated_at: "",
-  });
-
-
+// export const Single = (props: Props) => {
+//   const [product, setProduct] = useState({
+//     id: "",
+//     name:"",
+//     code: "",
+//     category_id: "",
+//     price: "",
+//     cost:"",
+//     qty: "",
+//     alert_quantity: "",
+//     image: "",
+//     description: "",
+//     is_active: false,
+//     created_at: "",
+//     updated_at: "",
+//   });
 
 
-  const [editingProduct, setEditingProduct] = useState({ ...product });
-  const [snackbarMessage, setSnackbarMessage] = useState("");
-  useEffect(() => {
-    getData(`${PRODUCT_BASE_URL}/api/v1/products/${props.id}`).then(
-      (res: any) => {
-        if (res.status) {
-          setProduct(res.data);
-          setEditingProduct(res.data);
+
+
+//   const [editingProduct, setEditingProduct] = useState({ ...product });
+//   const [snackbarMessage, setSnackbarMessage] = useState("");
+//   useEffect(() => {
+//     getData(`${PRODUCT_BASE_URL}/api/v1/products/${props.id}`).then(
+//       (res: any) => {
+//         if (res.status) {
+//           setProduct(res.data);
+//           setEditingProduct(res.data);
        
 
-        }else{
-          console.log(res.message)
-        }
-      }
-    );
-  }, []);
+//         }else{
+//           console.log(res.message)
+//         }
+//       }
+//     );
+//   }, []);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setEditingProduct({ ...editingProduct, [name]: value });
-  };
+//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setEditingProduct({ ...editingProduct, [name]: value });
+//   };
 
-  const handleUpdateProduct = () => {
-    putData(`${PRODUCT_BASE_URL}/api/v1/products/${props.id}`, editingProduct)
-      .then((res: any) => {
-        if (res.status) {
-          setSnackbarMessage("Product updated successfully.");
-        }
-        console.log(res.message)
-      })
-      .catch((error: any) => {
-        setSnackbarMessage("Failed to update product. Please try again.");
-      });
-  };
-  const handleCloseSnackbar = () => {
-    setSnackbarMessage("");
-  };
-  return (
-    <div className="single">
-      <div className="view">
-        <div className="info">
-          <div className="details">
-            <div className="item">
-              <span className="itemTitle">Name:</span>
-              <input
-                type="text"
-                name="name"
-                value={editingProduct.name}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="item">
-              <span className="itemTitle">Code:</span>
-              <input
-                type="text"
-                name="code"
-                value={editingProduct.code}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="item">
-              <span className="itemTitle">Category ID:</span>
-              <input
-                type="text"
-                name="category_id"
-                value={editingProduct.category_id}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="item">
-              <span className="itemTitle">Price:</span>
-              <input
-                type="text"
-                name="price"
-                value={editingProduct.price}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="item">
-              <span className="itemTitle">Cost:</span>
-              <input
-                type="text"
-                name="cost"
-                value={editingProduct.cost}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="item">
-              <span className="itemTitle">Alert Quantity:</span>
-              <input
-                type="text"
-                name="alert_quantity"
-                value={editingProduct.alert_quantity}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="item">
-              <span className="itemTitle">Description:</span>
-              <input
-                type="text"
-                name="description"
-                value={editingProduct.description}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="bottomInfo">
-            <button className="updateButton" onClick={handleUpdateProduct}>
-              Update
-            </button>
-          </div>
-        </div>
-        <div className="image">
+//   const handleUpdateProduct = () => {
+//     putData(`${PRODUCT_BASE_URL}/api/v1/products/${props.id}`, editingProduct)
+//       .then((res: any) => {
+//         if (res.status) {
+//           setSnackbarMessage("Product updated successfully.");
+//         }
+//         console.log(res.message)
+//       })
+//       .catch((error: any) => {
+//         setSnackbarMessage("Failed to update product. Please try again.");
+//       });
+//   };
+//   const handleCloseSnackbar = () => {
+//     setSnackbarMessage("");
+//   };
+//   return (
+//     <div className="single">
+//       <div className="view">
+//         <div className="info">
+//           <div className="details">
+//             <div className="item">
+//               <span className="itemTitle">Name:</span>
+//               <input
+//                 type="text"
+//                 name="name"
+//                 value={editingProduct.name}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div className="item">
+//               <span className="itemTitle">Code:</span>
+//               <input
+//                 type="text"
+//                 name="code"
+//                 value={editingProduct.code}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div className="item">
+//               <span className="itemTitle">Category ID:</span>
+//               <input
+//                 type="text"
+//                 name="category_id"
+//                 value={editingProduct.category_id}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div className="item">
+//               <span className="itemTitle">Price:</span>
+//               <input
+//                 type="text"
+//                 name="price"
+//                 value={editingProduct.price}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div className="item">
+//               <span className="itemTitle">Cost:</span>
+//               <input
+//                 type="text"
+//                 name="cost"
+//                 value={editingProduct.cost}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div className="item">
+//               <span className="itemTitle">Alert Quantity:</span>
+//               <input
+//                 type="text"
+//                 name="alert_quantity"
+//                 value={editingProduct.alert_quantity}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//             <div className="item">
+//               <span className="itemTitle">Description:</span>
+//               <input
+//                 type="text"
+//                 name="description"
+//                 value={editingProduct.description}
+//                 onChange={handleInputChange}
+//               />
+//             </div>
+//           </div>
+//           <div className="bottomInfo">
+//             <button className="updateButton" onClick={handleUpdateProduct}>
+//               Update
+//             </button>
+//           </div>
+//         </div>
+//         <div className="image">
         
-          <img
-            src={`data:image/jpeg;base64,${product.image}`}
-            alt=""
-          />
-        </div>
-      </div>
-      <Snackbar
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        open={!!snackbarMessage}
-        autoHideDuration={6000}
-        onClose={handleCloseSnackbar}
-        message={snackbarMessage}
-        action={
-          <React.Fragment>
-            <IconButton
-              size="small"
-              aria-label="close"
-              color="inherit"
-              onClick={handleCloseSnackbar}
-            >
-              {/* <CloseIcon fontSize="small" /> */}
-            </IconButton>
-          </React.Fragment>
-        }
-      />
-    </div>
-  );
-};
+//           <img
+//             src={`data:image/jpeg;base64,${product.image}`}
+//             alt=""
+//           />
+//         </div>
+//       </div>
+//       <Snackbar
+//         anchorOrigin={{
+//           vertical: "bottom",
+//           horizontal: "left",
+//         }}
+//         open={!!snackbarMessage}
+//         autoHideDuration={6000}
+//         onClose={handleCloseSnackbar}
+//         message={snackbarMessage}
+//         action={
+//           <React.Fragment>
+//             <IconButton
+//               size="small"
+//               aria-label="close"
+//               color="inherit"
+//               onClick={handleCloseSnackbar}
+//             >
+//               {/* <CloseIcon fontSize="small" /> */}
+//             </IconButton>
+//           </React.Fragment>
+//         }
+//       />
+//     </div>
+//   );
+// };
--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,13 +1,174 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./product.scss";
-import { Single } from "../../components/single/Single";
+
 import { useParams } from "react-router-dom";
+import { IconButton, Snackbar } from "@mui/material";
+import { PRODUCT_BASE_URL } from "../../data";
+import { getData, putData } from "../../components/API/HttpService";
 
 export const Product = () => {
   const { id } = useParams<{ id: string }>();
+  const [product, setProduct] = useState({
+    id: "",
+    name:"",
+    code: "",
+    category_id: "",
+    price: "",
+    cost:"",
+    qty: "",
+    alert_quantity: "",
+    image: "",
+    description: "",
+    is_active: false,
+    created_at: "",
+    updated_at: "",
+  });
+
+
+
+
+  const [editingProduct, setEditingProduct] = useState({ ...product });
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  useEffect(() => {
+    getData(`${PRODUCT_BASE_URL}/api/v1/products/${id}`).then(
+      (res: any) => {
+        if (res.status) {
+          setProduct(res.data);
+          setEditingProduct(res.data);
+       
+
+        }else{
+          console.log(res.message)
+        }
+      }
+    );
+  }, []);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setEditingProduct({ ...editingProduct, [name]: value });
+  };
+
+  const handleUpdateProduct = () => {
+    putData(`${PRODUCT_BASE_URL}/api/v1/products/${id}`, editingProduct)
+      .then((res: any) => {
+        if (res.status) {
+          setSnackbarMessage("Product updated successfully.");
+        }
+        console.log(res.message)
+      })
+      .catch((error: any) => {
+        setSnackbarMessage("Failed to update product. Please try again.");
+      });
+  };
+  const handleCloseSnackbar = () => {
+    setSnackbarMessage("");
+  };
   return (
-    <div className="product">
-      <Single id={id ?? ""} />
+    <div className="single">
+      <div className="view">
+        <div className="info">
+          <div className="details">
+            <div className="item">
+              <span className="itemTitle">Name:</span>
+              <input
+                type="text"
+                name="name"
+                value={editingProduct.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="item">
+              <span className="itemTitle">Code:</span>
+              <input
+                type="text"
+                name="code"
+                value={editingProduct.code}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="item">
+              <span className="itemTitle">Category ID:</span>
+              <input
+                type="text"
+                name="category_id"
+                value={editingProduct.category_id}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="item">
+              <span className="itemTitle">Price:</span>
+              <input
+                type="text"
+                name="price"
+                value={editingProduct.price}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="item">
+              <span className="itemTitle">Cost:</span>
+              <input
+                type="text"
+                name="cost"
+                value={editingProduct.cost}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="item">
+              <span className="itemTitle">Alert Quantity:</span>
+              <input
+                type="text"
+                name="alert_quantity"
+                value={editingProduct.alert_quantity}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="item">
+              <span className="itemTitle">Description:</span>
+              <input
+                type="text"
+                name="description"
+                value={editingProduct.description}
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+          <div className="bottomInfo">
+            <button className="updateButton" onClick={handleUpdateProduct}>
+              Update
+            </button>
+          </div>
+        </div>
+        <div className="image">
+        
+          <img
+            src={`data:image/jpeg;base64,${product.image}`}
+            alt=""
+          />
+        </div>
+      </div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        open={!!snackbarMessage}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        message={snackbarMessage}
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleCloseSnackbar}
+            >
+              {/* <CloseIcon fontSize="small" /> */}
+            </IconButton>
+          </React.Fragment>
+        }
+      />
     </div>
   );
 };
